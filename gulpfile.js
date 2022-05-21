6e1cdc731e7847acc182e07738620ae07a6974f8
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2022-05-21 12:57:19 -0300

    Inicio do projeto Brafé

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+const { src, dest, series } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+function buildStyles() {
+  return gulp
+    .src('./css/scss/*.scss')
+    .pipe(sass.sync({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+}
+
+exports.buildStyles = buildStyles;
+
+function watch() {
+  gulp.watch('./css/scss/*.scss', buildStyles);
+}
+
+// Static server
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+});
+
+const start = gulp.series(buildStyles, watch);
+exports.default = start;
