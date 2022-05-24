836fee7d1eca6d06d622310d64d5f126c4534f5f
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2022-05-24 14:24:42 -0300

    Criando evento pra aumentar tamanho do texto

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const produtos = document.querySelectorAll('.produtos .produtos-container div');
 produtos.forEach((produto) => {
   let classe = produto.querySelector('h2').innerText.toLowerCase();
   produto.classList.add(classe);
-  console.log(classe);
+  //console.log(classe);
 });
 //console.log(produtos);
+
+const btnAumentar = document.querySelector('#aumentar');
+
+function aumentarTexto() {
+  const p = document.querySelectorAll('p');
+  p.forEach((texto) => {
+    texto.classList.add('texto-maior');
+  });
+}
+btnAumentar.addEventListener('click', aumentarTexto);
+console.log(btnAumentar);
